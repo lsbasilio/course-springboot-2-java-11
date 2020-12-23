c13e217d4b26e4774a726b69346ce3c9c9ba8b74
Leandro Basilio <leandro_sanzi@yahoo.com.br>
2020-12-23 16:50:11 -0300

    Order, Instant, ISO 8601 - PART 2

--- src/main/java/com/educandoweb/course/entities/User.java
+++ src/main/java/com/educandoweb/course/entities/User.java
@@ -11,6 +11,8 @@ import javax.persistence.Id;
 import javax.persistence.OneToMany;
 import javax.persistence.Table;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
 @Entity
 @Table(name = "tb_user")
 public class User implements Serializable {
@@ -25,6 +27,7 @@ public class User implements Serializable {
 	private String phone;
 	private String password;
 	
+	@JsonIgnore  // Evita looping infinito no JSon do Postman
 	@OneToMany(mappedBy= "client")
 	private List<Order> orders = new ArrayList<>();
 	
